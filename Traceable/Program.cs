5c77805a5b58574f38cc65265611def9ef0a1d0b
Jacksing <hijackyeah@163.com>
2016-07-27 18:58:26 +0800

    Added traceable module
    Support for data collection to combine and trace back.

---
+++ Traceable/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Traceable;
+
+namespace Traceable
+{
+    internal class Program
+    {
+        private static int _counter = 1;
+
+        private static void Main(string[] args)
+        {
+            #region allDataCollection
+            List<TestObj> allDataCollection = new List<TestObj>()
+            {
+                new TestObj()
+                {
+                    "A1",
+                    "A2",
+                    "A3",
+                    "A4",
+                },
+                new TestObj()
+                {
+                    "B1",
+                    "B2",
+                    "B3",
+                    "B4",
+                },
+                new TestObj()
+                {
+                    "C1",
+                    "C2",
+                    "C3",
+                    "C4",
+                },
+                new TestObj()
+                {
+                    "D1",
+                    "D2",
+                    "D3",
+                    "D4",
+                },
+                new TestObj()
+                {
+                    "E1",
+                    "E2",
+                    "E3",
+                    "E4",
+                },
+            }; 
+            #endregion
+
+            TestTracer tracer = new TestTracer();
+
+            // foward
+            while (true)
+            {
+                PrintStatus(allDataCollection, tracer);
+
+                var randomForward = RandomForward(allDataCollection, 4);
+
+                if (randomForward == null)
+                {
+                    break;
+                }
+
+                var forwarded = tracer.Forward(randomForward.ToList());
+                allDataCollection.Add(forwarded);
+            }
+
+            // backward
+            while (true)
+            {
+                if (!allDataCollection.Any(x => x.Traced != null))
+                {
+                    break;
+                }
+
+                List<TestObj> tracedCollection = new List<TestObj>();
+                foreach (var data in allDataCollection)
+                {
+                    if (data.Traced != null)
+                    {
+                        tracedCollection.AddRange(tracer.Backward(data));
+                    }
+                    else
+                    {
+                        tracedCollection.Add(data);
+                    }
+                }
+                allDataCollection = tracedCollection;
+
+                PrintStatus(allDataCollection, tracer);
+            }
+
+            Console.WriteLine("\r\nFinished, Enter to exit...");
+            Console.ReadLine();
+        }
+
+        private static List<TestObj> RandomForward(List<TestObj> original, int count)
+        {
+            if (original.Count == 1)
+            {
+                return null;
+            }
+
+            List<TestObj> ret = new List<TestObj>();
+
+            Random random = new Random();
+            TestObj removed = null;
+
+            for (int i = 0; i < count; i++)
+            {
+                if (original.Count == 1)
+                {
+                    removed = original[0];
+                    original.Remove(removed);
+                    ret.Add(removed);
+                    return ret;
+                }
+
+                removed = original[random.Next(0, original.Count)];
+                original.Remove(removed);
+                ret.Add(removed);
+            }
+
+            return ret;
+        }
+
+        private static void PrintStatus(List<TestObj> allDataCollection, TestTracer tracer)
+        {
+            Func<string, string> tracedMsg = x =>
+            {
+                return x == null ? "  null  " : x;
+            };
+
+            Console.WriteLine(_counter++.ToString() + ") --------------------");
+
+            Console.WriteLine(">> Current Data");
+
+            foreach (var data in allDataCollection)
+            {
+                var msg = string.Join("\t", data) + "\t" + tracedMsg(data.Traced);
+                Console.WriteLine(msg);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(">> Trace");
+
+            foreach (var trace in tracer.TraceCollection)
+            {
+//                Console.WriteLine(trace.Key);
+
+                foreach (var data in trace.Value)
+                {
+                    var msg = string.Join("\t", data) + "\t" + tracedMsg(data.Traced) + "\t" + trace.Key;
+                    Console.WriteLine(msg);
+                }
+
+//                Console.WriteLine();
+            }
+
+            Console.WriteLine("\r\n\r\n");
+        }
+    }
+}
---
+++ Traceable/Tracer.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Traceable
+{
+    public static class DictionaryExtend
+    {
+        public static TValue Get<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key, TValue defaultValue)
+        {
+            TValue ret;
+            if (dict.TryGetValue(key, out ret))
+            {
+                return ret;
+            }
+            else
+            {
+                return defaultValue;
+            }
+        }
+    }
+
+    public interface TraceableObject
+    {
+        string Traced { get; set; }
+    }
+
+    public abstract class Tracer<T> where T : TraceableObject
+    {
+        private Dictionary<string, List<T>> _traceCollection = new Dictionary<string, List<T>>();
+
+        public Dictionary<string, List<T>> TraceCollection
+        {
+            get { return _traceCollection; }
+        }
+
+        // Get all trace data that linked to trace.
+        protected virtual List<T> GetTrace(string traceid)
+        {
+            var trace = _traceCollection.Get(traceid, null);
+            if (trace != null)
+            {
+                _traceCollection.Remove(traceid);
+            }
+            return trace;
+        }
+
+        protected virtual string NewTraceId()
+        {
+            return Guid.NewGuid().ToString();
+        }
+
+        // Create a new trace id and make all the trace data
+        // linked to it, then save these linked trace data.
+        // With the trace id all saved trace data can be found
+        // by calling `GetTrace()` in the future.
+        //
+        // After all returns the trace id.
+        protected virtual string SaveTrace(List<T> objList)
+        {
+            var traceId = this.NewTraceId();
+            _traceCollection.Add(traceId, objList);
+            return traceId;
+        }
+
+        // Combine all trace data into one new trace data.
+        protected abstract T DoForward(List<T> objList);
+
+        public virtual T Forward(List<T> objList)
+        {
+            var traceId = this.SaveTrace(objList);
+            var newObj = this.DoForward(objList.ToList());
+            newObj.Traced = traceId;
+
+            return newObj;
+        }
+
+        public T Forward(params T[] objArray)
+        {
+            return this.Forward(objArray.ToList());
+        }
+
+        public virtual List<T> Backward(T obj)
+        {
+            return this.GetTrace(obj.Traced);
+        }
+    }
+
+    /////////////////////////////////////////////////////////////
+    /// basic test
+    /////////////////////////////////////////////////////////////
+
+    public class TestObj : List<string>, TraceableObject
+    {
+        public string Traced { get; set; }
+    }
+
+    public class TestTracer : Tracer<TestObj>
+    {
+        protected override string NewTraceId()
+        {
+            return base.NewTraceId().Split('-')[0];
+        }
+
+        protected override TestObj DoForward(List<TestObj> objList)
+        {
+            if (objList == null || objList.Count == 0)
+            {
+                return null;
+            }
+
+            var maxCount = objList.Max(x => x.Count);
+            TestObj ret = new TestObj();
+            ret.AddRange(Enumerable.Repeat("", maxCount));
+
+            objList.ForEach(x =>
+            {
+                for (int i = 0; i < x.Count; i++)
+                {
+                    ret[i] = ret[i] + x[i];
+                }
+            });
+
+            return ret;
+        }
+    }
+}
